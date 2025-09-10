eef6e1e071fc2533856687a08b89ea9aa0138e34
Volodymyr Vyshko <volodymyr.vyshko@gmail.com>
2025-09-11 00:45:04 +0300

    chore: cleanup

--- src/app.service.ts
+++ src/app.service.ts
@@ -3,6 +3,6 @@ import { Injectable } from '@nestjs/common';
 @Injectable()
 export class AppService {
   getHello(data: object): string {
-    return `Hello, passed info: ${JSON.stringify(data)}`;
+    return `User info: ${JSON.stringify(data)}`;
   }
 }
