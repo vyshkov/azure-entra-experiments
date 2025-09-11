968bbb79244235c4cb594b4a6f0fedd944d0ee27
Volodymyr Vyshko <volodymyr.vyshko@gmail.com>
2025-09-11 13:28:54 +0300

    fix: allow cors for testing

--- src/main.ts
+++ src/main.ts
@@ -3,6 +3,14 @@ import { AppModule } from './app.module';
 
 async function bootstrap() {
   const app = await NestFactory.create(AppModule);
+
+  // Allow ANY origin
+  app.enableCors({
+    origin: '*', // 🚨 allows all origins
+    methods: 'GET,HEAD,PUT,PATCH,POST,DELETE,OPTIONS',
+    allowedHeaders: '*',
+  });
+
   await app.listen(process.env.PORT || 8080);
 }
 bootstrap();
