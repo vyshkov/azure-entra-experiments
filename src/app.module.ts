1a424bf382af12884f74a4f19dae1faf86262f88
Volodymyr Vyshko <volodymyr.vyshko@gmail.com>
2025-09-11 13:48:57 +0300

    fix: updated auth middleware

--- src/app.module.ts
+++ src/app.module.ts
@@ -1,12 +1,14 @@
 import { Module } from '@nestjs/common';
 import { AppController } from './app.controller';
 import { AppService } from './app.service';
+import { JwtStrategy } from './auth/jwt.strategy';
+import { PassportModule } from '@nestjs/passport';
 //import { EasyAuthMiddleware } from './auth/easy-auth.middleware';
 
 @Module({
-  imports: [],
+  imports: [PassportModule.register({ defaultStrategy: 'azure-ad' })],
   controllers: [AppController],
-  providers: [AppService],
+  providers: [AppService, JwtStrategy],
 })
 export class AppModule {
   // configure(consumer: MiddlewareConsumer) {
