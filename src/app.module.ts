fb4b3f241b557cb043ac9b09751130dca5af346a
Volodymyr Vyshko <volodymyr.vyshko@gmail.com>
2025-09-11 16:04:08 +0300

    chore: cleanup the code

--- src/app.module.ts
+++ src/app.module.ts
@@ -3,15 +3,10 @@ import { AppController } from './app.controller';
 import { AppService } from './app.service';
 import { JwtStrategy } from './auth/jwt.strategy';
 import { PassportModule } from '@nestjs/passport';
-//import { EasyAuthMiddleware } from './auth/easy-auth.middleware';
 
 @Module({
   imports: [PassportModule.register({ defaultStrategy: 'azure-ad' })],
   controllers: [AppController],
   providers: [AppService, JwtStrategy],
 })
-export class AppModule {
-  // configure(consumer: MiddlewareConsumer) {
-  // // consumer.apply(EasyAuthMiddleware).forRoutes('*');
-  // }
-}
+export class AppModule {}
--- src/auth/jwt.strategy.ts
+++ src/auth/jwt.strategy.ts
@@ -12,7 +12,7 @@ export class JwtStrategy extends PassportStrategy(BearerStrategy, 'azure-ad') {
       identityMetadata: `https://login.microsoftonline.com/060e0979-b29c-46cc-9075-0c0478bffd3e/v2.0/.well-known/openid-configuration`,
       clientID: '2b9758a9-513d-46df-bdc4-b16117654611', // backend App Registration ID
       audience: 'api://2b9758a9-513d-46df-bdc4-b16117654611',
-      validateIssuer: false,
+      validateIssuer: true,
       passReqToCallback: false,
       loggingLevel: 'info',
     });
