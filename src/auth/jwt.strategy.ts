d1d789d1c7cfd96bef46dc0d030da301c22159a7
Volodymyr Vyshko <volodymyr.vyshko@gmail.com>
2025-09-11 14:08:17 +0300

    chore: change login config

--- src/auth/jwt.strategy.ts
+++ src/auth/jwt.strategy.ts
@@ -1,3 +1,5 @@
+/* eslint-disable @typescript-eslint/no-unsafe-call */
+/* eslint-disable @typescript-eslint/require-await */
 /* eslint-disable @typescript-eslint/no-unsafe-return */
 import { Injectable } from '@nestjs/common';
 import { PassportStrategy } from '@nestjs/passport';
@@ -6,19 +8,18 @@ import { BearerStrategy } from 'passport-azure-ad';
 @Injectable()
 export class JwtStrategy extends PassportStrategy(BearerStrategy, 'azure-ad') {
   constructor() {
-    // eslint-disable-next-line @typescript-eslint/no-unsafe-call
     super({
       identityMetadata: `https://login.microsoftonline.com/060e0979-b29c-46cc-9075-0c0478bffd3e/v2.0/.well-known/openid-configuration`,
-      clientID: '2b9758a9-513d-46df-bdc4-b16117654611', // Backend App Registration (Application ID)
+      clientID: '2b9758a9-513d-46df-bdc4-b16117654611', // backend App Registration ID
       audience: 'api://2b9758a9-513d-46df-bdc4-b16117654611',
-      validateIssuer: true,
+      validateIssuer: false,
       passReqToCallback: false,
       loggingLevel: 'info',
     });
   }
 
-  validate(token: any) {
-    // Passport-Azure-AD already validates signature, audience, issuer
-    return token; // This will be attached to req.user
+  async validate(payload: any) {
+    console.log('✅ Validated token payload:', payload);
+    return payload;
   }
 }
