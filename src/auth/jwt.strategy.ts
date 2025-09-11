6c48d3d2738257948421bb59dc2ec3adf3bf71ad
Volodymyr Vyshko <volodymyr.vyshko@gmail.com>
2025-09-11 16:33:41 +0300

    fix: change issuer version

--- src/auth/jwt.strategy.ts
+++ src/auth/jwt.strategy.ts
@@ -10,13 +10,12 @@ import { BACKEND_CLIENT_ID, TENANT_ID } from 'src/constants';
 export class JwtStrategy extends PassportStrategy(BearerStrategy, 'azure-ad') {
   constructor() {
     super({
-      identityMetadata: `https://login.microsoftonline.com/${TENANT_ID}/v2.0/.well-known/openid-configuration`,
+      identityMetadata: `https://login.microsoftonline.com/${TENANT_ID}/.well-known/openid-configuration`,
       clientID: BACKEND_CLIENT_ID, // backend App Registration ID
       audience: `api://${BACKEND_CLIENT_ID}`,
       validateIssuer: true,
       passReqToCallback: false,
       loggingLevel: 'info',
-      issuer: `https://login.microsoftonline.com/${TENANT_ID}/v2.0`,
     });
   }
 
