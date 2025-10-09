1e8607b595371ef5c03810cda4d3320b5742871b
Volodymyr Vyshko <volodymyr.vyshko@gmail.com>
2025-10-09 14:18:32 +0300

    Update constants.ts

--- src/constants.ts
+++ src/constants.ts
@@ -1,2 +1,2 @@
-export const TENANT_ID = '060e0979-b29c-46cc-9075-0c0478bffd3e';
-export const BACKEND_CLIENT_ID = '2b9758a9-513d-46df-bdc4-b16117654611';
+export const TENANT_ID = '493d0624-0dfc-4539-bcf8-73eeec22c9a0';
+export const BACKEND_CLIENT_ID = '93afd969-2de6-452b-8f2b-70f437b86c70';
